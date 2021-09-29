99325082ba4ec2d027f92bbecedb1004a4e5087d
bayu <bayu23@gmail.com>
2021-09-29 22:08:51 +0700

    androidstudio

---
+++ RelativeLayout/app/src/test/java/com/bayu/veActivity.java
@@ -0,0 +1,65 @@
+package com.bayu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Calendar;
+
+public class RelativeLayout extends AppCompatActivity {
+    private static final android.R.attr R = ;
+    DatePickerDialog picker;
+    EditText eText;
+    Button btnGet;
+    TextView tvw;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super,onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvw=(TextView) findViewById(R.id.textView1);
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar cldr= Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                //date picker dialog
+                picker =new DatePickerDialog( contex: RelativeLayout.this,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                                  int dayOfMonth) {
+                                eText.setText((dayOfMonth + "/" + (monthOfYear + 1)+"/"+year);
+                            }
+                        }, year, month, day;
+                picker.show();
+            }
+        });
+        btnGet=(Button) findViewById(R.id,button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText("selected Date+ eText.gettext());
+            }
+        });
+    }
+}
+
+
